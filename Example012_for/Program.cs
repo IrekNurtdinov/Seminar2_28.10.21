630118c65b86e10fa27cb5a23893f3ca7332ab39
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-06 20:31:23 +0300

    doing exercises with methods

--- Example012_for/Program.cs
+++ Example012_for/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int[] arr = new int[10];
+
+// int index = 0;
+
+// while (index < 10)
+// {
+//      Console.WriteLine($"{arr[index++]}");
+     
+// }
+
+void DrawRectangle(int posX, int posY, int width, int height )
+{
+
+    Console.SetCursorPosition(posX, posY);
+    int count = 0;
+    while (count < width)
+    {
+       Console.Write("+"); 
+       count++; 
+       
+    }
+    
+}
+
+DrawRectangle(20, 10, 6, 2);

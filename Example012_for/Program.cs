4d6aa14871b885366560b96660d4d27c060bce22
Irek Nurtdinov <nurtdinov.irek@gmail.com>
2021-11-07 14:23:49 +0300

    soving exercices

--- Example012_for/Program.cs
+++ Example012_for/Program.cs
@@ -8,18 +8,76 @@
      
 // }
 
-void DrawRectangle(int posX, int posY, int width, int height )
-{
+//Рисуем прямоугольник
 
-    Console.SetCursorPosition(posX, posY);
-    int count = 0;
-    while (count < width)
-    {
-       Console.Write("+"); 
-       count++; 
+// void DrawRectangle(int posX, int posY, int width, int height )
+// {
+// int row = 0;
+// while (row < height)
+// {
+//     Console.SetCursorPosition(posX, posY+row);
+//     int count = 0;
+//     while (count < width)
+//     {
+//        Console.Write("+"); 
+//        count++; 
        
-    }
-    
-}
+//     }
+//     row++;
+// }
+// }
+// DrawRectangle(5, 5, 9, 4);
 
-DrawRectangle(20, 10, 6, 2);
+// Нужно написать рограмму решения квадратных уравнений
+
+// Ax^2 + Bx + C = 0
+// 1x^2 - 2x + 6 = 0
+
+// Метод еоторый не возвращает:
+
+// void Solve(double a, double b, double c) {
+    
+//     double d = b * b - 4 * a * c; // дискриминант
+//     Console.WriteLine(d);
+
+//     if (d < 0)
+//     {
+//         Console.WriteLine("Действительных корней нет");
+//     }
+//     else 
+//     {
+//         double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+//         double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+//         Console.WriteLine(x1);
+//         Console.WriteLine(x2);
+
+//     }
+//     Console.WriteLine();
+//     }
+
+
+// Метод который возвращает:   
+(double, double) Solve(double a, double b, double c) {
+    
+    double d = b * b - 4 * a * c; // дискриминант
+    Console.WriteLine(d);
+
+    if (d < 0)
+    {
+        //Console.WriteLine("Действительных корней нет");
+        return (double.NaN, double.NaN);
+    }
+    else 
+    {
+        double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+        double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+        return (x1, x2);
+
+    }
+    Console.WriteLine();
+    }
+
+(double, double) roots = Solve(1, -7, 6); Console.WriteLine(roots);   
+roots = Solve(1, 0, 6); Console.WriteLine(roots); 
+roots = Solve(1, 0, -4); Console.WriteLine(roots); 
+roots = Solve(1, 2, -4); Console.WriteLine(roots); 
